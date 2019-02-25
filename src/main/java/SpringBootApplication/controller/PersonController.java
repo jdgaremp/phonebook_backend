a2dba158d723071c0566a3abcd8accf0140fd1c7
Juliette de Bressieux <jdb@wemanity.com>
2019-02-25 14:07:25 +0100

    Update PersonController.java.

--- src/main/java/SpringBootApplication/controller/PersonController.java
+++ src/main/java/SpringBootApplication/controller/PersonController.java
@@ -2,10 +2,8 @@ package SpringBootApplication.controller;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import SpringBootApplication.model.Person;
 import SpringBootApplication.service.PersonService;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4400")
 @RestController
 @RequestMapping("/persons")
 public class PersonController {
@@ -58,7 +56,7 @@ public class PersonController {
 		return personFound;
 	}
 
-	@CrossOrigin(origins = "http://localhost:4200")
+	@CrossOrigin(origins = "http://localhost:4400")
 	@GetMapping("/search")
 	public List<Person> search(@RequestParam(value = FIRST_NAME, required = false) String firstName,
 			@RequestParam(value = LAST_NAME, required = false) String lastName,
